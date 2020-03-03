b5d907fbda28dbc481ac977b5aa5baa36312decf
tiepkhactran <tiepkhactran@users.noreply.github.com>
2020-03-04 00:27:49 +0700

    had

--- Front_end_dinh_van_hoang_an/App_Start/BundleConfig.cs
+++ Front_end_dinh_van_hoang_an/App_Start/BundleConfig.cs
@@ -25,6 +25,42 @@ namespace Front_end_dinh_van_hoang_an
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Css").Include(
+                 "~/Content/css/font-awesome.min.css",
+                 "~/Content/css/owl.carousel.css",
+                 "~/Content/css/owl.my_theme.css",
+                 "~/Content/css/owl.transitions.css",
+                 "~/Content/css/nivo-slider.css",
+                 "~/Content/css/animate.css",
+                 "~/Content/css/jquery-ui.css",
+                 "~/Content/css/jquery.fancybox.css",
+                 "~/Content/css/normalize.css",
+                 "~/Content/css/bootstrap.min.css",
+                 "~/Content/css/custom.css",
+                 "~/Content/css/meanmenu.min.css",
+                 "~/Content/css/responsive.css",
+                 "~/Content/css/main.css",
+                 "~/Content/css/style.css"
+                 ));
+            bundles.Add(new ScriptBundle("~/Js").Include(
+                "~/Scripts/js-frontend/vendor/modernizr-2.8.3.min.js",
+                "~/Scripts/js-frontend/vendor/jquery-1.11.3.min.js",
+                "~/Scripts/js-frontend/bootstrap.min.js",
+                "~/Scripts/js-frontend/jquery.meanmenu.js",
+                "~/Scripts/js-frontend/jquery.easing.1.3.min.js",
+                "~/Scripts/js-frontend/jquery.knob.js",
+                "~/Scripts/js-frontend/jquery.fancybox.pack.js",
+                "~/Scripts/js-frontend/price-slider.js",
+                "~/Scripts/js-frontend/jquery.nivo.slider.pack.js",
+                "~/Scripts/js-frontend/wow.js",
+                "~/Scripts/js-frontend/nivo-plugin.js",
+                "~/Scripts/js-frontend/jquery.scrollUp.js",
+                "~/Scripts/js-frontend/owl.carousel.min.js",
+                "~/Scripts/js-frontend/plugins.js",
+                "~/Scripts/js-frontend/main.js",
+                "~/Scripts/js-frontend/jquery.appear.js"
+                ));
         }
     }
 }
