4feca618a415e882588c8115887e8ee60b149137
tiepkhactran <tiepkhactran@users.noreply.github.com>
2020-03-03 22:43:55 +0700

    front-end

---
+++ Front_end_dinh_van_hoang_an/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Front_end_dinh_van_hoang_an
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Front_end_dinh_van_hoang_an/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Front_end_dinh_van_hoang_an.MvcApplication" Language="C#" %>
